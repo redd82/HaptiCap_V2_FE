bc186bf824a3c74bf686acd3fdda66fec497d46a
Tristan Daams <t.r.daams@tudelft.nl>
2023-02-06 20:34:21 +0100

    8

--- src/app/pages/map/UseMap.js
+++ src/app/pages/map/UseMap.js
@@ -5,6 +5,7 @@ export default function UseMap({title, imagesource}){
 
     return(
         <div>
+            <h1>{title}</h1>
             <img className={styles['map']} src={imagesource} usemap='#gridmap' alt=""></img>
         </div>
     );
